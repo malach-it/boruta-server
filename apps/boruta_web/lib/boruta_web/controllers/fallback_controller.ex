68899e30cc8a2a678415315ecd802ef39b5c6daa
Pascal Knoth <pascal.knoth@gmx.com>
2019-05-26 20:55:26 +0200

    cleanup

--- apps/boruta_web/lib/boruta_web/controllers/fallback_controller.ex
+++ apps/boruta_web/lib/boruta_web/controllers/fallback_controller.ex
@@ -12,18 +12,4 @@ defmodule BorutaWeb.FallbackController do
     |> put_view(BorutaWeb.ErrorView)
     |> render(:"404")
   end
-
-  def call(conn, {:error, %{invalid_client: error_description}, :unauthorized}) do
-    conn
-    |> put_status(:unauthorized)
-    |> put_view(BorutaWeb.OauthView)
-    |> render("error." <> get_format(conn), error: "invalid_client", error_description: error_description)
-  end
-
-  def call(conn, {:error, %{invalid_scope: error_description}, :bad_request}) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(BorutaWeb.OauthView)
-    |> render("error." <> get_format(conn), error: "invalid_scope", error_description: error_description)
-  end
 end
