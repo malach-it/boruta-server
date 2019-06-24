38bfa1255656f41f33f5c828254869e730d93d5a
Pascal Knoth <pascal.knoth@gmx.com>
2019-06-24 22:18:30 +0200

    improve resource owner documentation

--- apps/boruta/lib/boruta/oauth/schemas/resource_owner.ex
+++ apps/boruta/lib/boruta/oauth/schemas/resource_owner.ex
@@ -1,6 +1,14 @@
 defmodule Boruta.Oauth.ResourceOwner do
   @moduledoc """
   Resource owner behaviour
+
+  Implement this behaviour for OAuth resource owners. It will be used in flows interacting with resource owners in two manners :
+  - by identifying them from the parameters given during the request lifecycle in implicit or code grants
+  - by challenging the couple email/password given by the client in resource owner password credentials
+
+  In order to do that resource owners must have an Ecto schema with `id`, `password_hash` entries and following callback. Providing a resource owner during request can be done by assigning `current_user` in `Plug.Conn` struct.
+
+  They will be then linked to the provided token.
   """
 
   @doc """
