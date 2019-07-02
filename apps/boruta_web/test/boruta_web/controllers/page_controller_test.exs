886e12b5977d1a9bac174973d134b671bba99045
Pascal Knoth <pascal.knoth@gmx.com>
2019-07-03 01:45:58 +0200

    first shot of an home page

--- apps/boruta_web/test/boruta_web/controllers/page_controller_test.exs
+++ apps/boruta_web/test/boruta_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BorutaWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Boruta"
   end
 end
