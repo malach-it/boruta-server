94cbceb5b884d3b664311ec2277b0e51dd8d51ed
Pascal Knoth <pascal.knoth@gmx.com>
2019-12-01 23:16:42 +0100

    revoke integration tests

--- apps/boruta_web/lib/boruta_web/controllers/oauth_controller.ex
+++ apps/boruta_web/lib/boruta_web/controllers/oauth_controller.ex
@@ -136,8 +136,16 @@ defmodule BorutaWeb.OauthController do
     conn |> Oauth.revoke(__MODULE__)
   end
 
+  @impl Boruta.OAuth.Application
   def revoke_success(%Plug.Conn{} = conn) do
     send_resp(conn, 200, "")
+  end
+  @impl Boruta.OAuth.Application
+  def revoke_error(conn, %Error{status: status, error: error, error_description: error_description}) do
+    conn
+    |> put_status(status)
+    |> put_view(OauthView)
+    |> render("error.json", error: error, error_description: error_description)
   end
 
   defp store_oauth_request(conn, params) do
--- apps/boruta_web/test/boruta_web/controllers/oauth_controller_test.exs
+++ apps/boruta_web/test/boruta_web/controllers/oauth_controller_test.exs
@@ -3,6 +3,8 @@ defmodule BorutaWeb.OauthControllerTest do
 
   import Boruta.Factory
 
+  alias Boruta.Token
+
   setup %{conn: conn} do
     {:ok, conn: conn}
   end
@@ -448,7 +450,7 @@ defmodule BorutaWeb.OauthControllerTest do
       assert json_response(conn, 200) == %{"active" => false}
     end
 
-    test "returns an introspect token response if client token is invalid", %{conn: conn, client: client, client_token: token} do
+    test "returns an introspect token response if client, token are valid", %{conn: conn, client: client, client_token: token} do
       conn = post(
         conn,
         "/oauth/introspect",
@@ -467,7 +469,7 @@ defmodule BorutaWeb.OauthControllerTest do
       }
     end
 
-    test "returns an introspect token response if resource owner token is invalid", %{conn: conn, client: client, resource_owner_token: token, resource_owner: resource_owner} do
+    test "returns an introspect token response if resource owner token is valid", %{conn: conn, client: client, resource_owner_token: token, resource_owner: resource_owner} do
       conn = post(
         conn,
         "/oauth/introspect",
@@ -484,6 +486,76 @@ defmodule BorutaWeb.OauthControllerTest do
         "sub" => resource_owner.id,
         "username" => resource_owner.email
       }
+    end
+  end
+
+  describe "revoke" do
+    setup %{conn: conn} do
+      client = insert(:client)
+      client_token = insert(:token, type: "access_token", value: "777", client_id: client.id)
+      resource_owner = insert(:user)
+      resource_owner_token = insert(:token, type: "access_token", value: "888", client_id: client.id, resource_owner_id: resource_owner.id)
+      {:ok,
+        conn: put_req_header(conn, "content-type", "application/x-www-form-urlencoded"),
+        client: client,
+        client_token: client_token,
+        resource_owner_token: resource_owner_token,
+        resource_owner: resource_owner
+      }
+    end
+
+    test "returns an error if request is invalid", %{conn: conn} do
+      conn = post(
+        conn,
+        "/oauth/revoke"
+      )
+      assert json_response(conn, 400) == %{
+        "error" => "invalid_request",
+        "error_description" => "Request validation failed. Required properties client_id, client_secret, token are missing at #."
+      }
+    end
+
+    test "returns an error if client is invalid", %{conn: conn, client: client} do
+      conn = post(
+        conn,
+        "/oauth/revoke",
+        "client_id=#{client.id}&client_secret=bad_secret&token=token"
+      )
+
+      assert json_response(conn, 401) == %{
+        "error" => "invalid_client",
+        "error_description" => "Invalid client_id or client_secret."
+      }
+    end
+
+    test "returns a success if token is invalid", %{conn: conn, client: client} do
+      conn = post(
+        conn,
+        "/oauth/revoke",
+        "client_id=#{client.id}&client_secret=#{client.secret}&token=bad_token"
+      )
+
+      assert response(conn, 200)
+    end
+
+    test "return a success if client, token are valid", %{conn: conn, client: client, client_token: token} do
+      conn = post(
+        conn,
+        "/oauth/revoke",
+        "client_id=#{client.id}&client_secret=#{client.secret}&token=#{token.value}"
+      )
+
+      assert response(conn, 200)
+    end
+
+    test "revoke token if client, token are valid", %{conn: conn, client: client, client_token: token} do
+      post(
+        conn,
+        "/oauth/revoke",
+        "client_id=#{client.id}&client_secret=#{client.secret}&token=#{token.value}"
+      )
+
+      assert Boruta.Repo.get_by(Token, value: token.value).revoked_at
     end
   end
 end
