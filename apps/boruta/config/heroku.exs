905b2ca8fc00fabffe65b585004e60054f72a561
Pascal Knoth <pascal.knoth@gmx.com>
2020-05-15 13:50:22 +0200

    heroku config

--- apps/boruta/config/heroku.exs
+++ apps/boruta/config/heroku.exs
@@ -1,7 +1,3 @@
-# Since configuration is shared in umbrella projects, this file
-# should only configure the :boruta application itself
-# and only for organization purposes. All other config goes to
-# the umbrella root.
 use Mix.Config
 
 config :boruta, Boruta.Repo,
