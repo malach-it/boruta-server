658b00541490fbfd97333083ba19cc66b5e14b7f
Pascal Knoth <pascal.knoth@gmx.com>
2020-06-02 22:48:25 +0200

    fix heroku configuration

--- apps/boruta_identity_provider/config/heroku.exs
+++ apps/boruta_identity_provider/config/heroku.exs
@@ -5,5 +5,5 @@ config :boruta_identity_provider, BorutaIdentityProvider.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-config :boruta_identityProvider, Boruta.Accounts,
+config :boruta_identity_provider, Boruta.Accounts,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
