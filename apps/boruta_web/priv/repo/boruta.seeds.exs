e7d7af5e8ca22afe2aedfd6b6e18261ea2a02058
Pascal Knoth <io.pascal.knoth@gmail.com>
2020-08-12 22:24:15 +0200

    heroku deployment

--- apps/boruta_web/priv/repo/boruta.seeds.exs
+++ apps/boruta_web/priv/repo/boruta.seeds.exs
@@ -1,12 +1,12 @@
 import Ecto.Changeset
 
-{:ok, scopes_scope} = BorutaWeb.Repo.insert(%Boruta.Ecto.Scope{
+{:ok, scopes_scope} = Boruta.Repo.insert(%Boruta.Ecto.Scope{
  name: "scopes:manage:all"
 })
-{:ok, clients_scope} = BorutaWeb.Repo.insert(%Boruta.Ecto.Scope{
+{:ok, clients_scope} = Boruta.Repo.insert(%Boruta.Ecto.Scope{
   name: "clients:manage:all"
 })
-{:ok, users_scope} = BorutaWeb.Repo.insert(%Boruta.Ecto.Scope{
+{:ok, users_scope} = Boruta.Repo.insert(%Boruta.Ecto.Scope{
   name: "users:manage:all"
 })
 
@@ -14,7 +14,7 @@ import Ecto.Changeset
 |> cast(%{
   id: "6a2f41a3-c54c-fce8-32d2-0324e1c32e20",
   secret: "777",
-  redirect_uris: ["http://localhost:4000/admin/oauth-callback"],
+  redirect_uris: ["https://boruta.herokuapp.com/admin/oauth-callback"],
   authorize_scope: false
 }, [:id, :secret, :redirect_uris, :authorize_scope])
-|> BorutaWeb.Repo.insert()
+|> Boruta.Repo.insert()
