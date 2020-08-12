1bad1e32e21e2f047eafe29d6f44b6c0a0044465
Pascal Knoth <io.pascal.knoth@gmail.com>
2020-08-12 22:24:15 +0200

    heroku deployment

--- apps/boruta_web/config/heroku.exs
+++ apps/boruta_web/config/heroku.exs
@@ -7,5 +7,10 @@ config :boruta_web, BorutaWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
+config :boruta_web, BorutaWeb.Repo,
+  ssl: true,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
 config :boruta_web, BorutaWeb.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
