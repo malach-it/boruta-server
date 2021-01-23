e3d4f90ff37aec45d3f2c97a28e04c6a32609d2b
Pascal Knoth <io.pascal.knoth@gmail.com>
2021-01-23 21:21:50 +0100

    update mix.lock + better config

--- apps/boruta_web/config/prod.exs
+++ apps/boruta_web/config/prod.exs
@@ -1,11 +1,1 @@
 use Mix.Config
-
-config :boruta_web, BorutaWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "admin.boruta.patatoid.fr"],
-  server: true,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
-
-config :boruta_web, BorutaWeb.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
