c56f4a83154a245f0b565e119be01ae8f7490621
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-03-30 21:36:27 +0200

    custom phoenix error pages

--- apps/boruta_admin/test/boruta_admin_web/views/error_view_test.exs
+++ apps/boruta_admin/test/boruta_admin_web/views/error_view_test.exs
@@ -5,10 +5,10 @@ defmodule BorutaAdminWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(BorutaAdminWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(BorutaAdminWeb.ErrorView, "404.html", []) =~ "Page not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(BorutaAdminWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(BorutaAdminWeb.ErrorView, "500.html", []) =~ "Internal server error"
   end
 end
--- apps/boruta_web/test/boruta_web/views/error_view_test.exs
+++ apps/boruta_web/test/boruta_web/views/error_view_test.exs
@@ -5,10 +5,10 @@ defmodule BorutaWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(BorutaWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(BorutaWeb.ErrorView, "404.html", []) =~ "Page not found"
   end
 
   test "renders 500.html" do
-    assert render_to_string(BorutaWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(BorutaWeb.ErrorView, "500.html", []) =~ "Internal server error"
   end
 end
