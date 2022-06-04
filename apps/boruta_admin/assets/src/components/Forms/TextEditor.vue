a1942239290e9249f42a9724b60c5331802fc8bb
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-06-04 23:00:34 +0200

    [admin] text editor font size

--- apps/boruta_admin/assets/src/components/Forms/TextEditor.vue
+++ apps/boruta_admin/assets/src/components/Forms/TextEditor.vue
@@ -54,7 +54,6 @@ export default {
       border-radius: 6px;
       box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
       font-family: 'Source Code Pro', monospace;
-      font-size: 1.3em;
       font-weight: 400;
       letter-spacing: normal;
       line-height: 20px;
