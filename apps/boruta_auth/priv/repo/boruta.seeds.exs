292ccc1ae4c69b69cf7adada695725c1cf8157f8
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-06-27 13:26:38 +0200

    rationalize seeds

--- apps/boruta_auth/priv/repo/boruta.seeds.exs
+++ apps/boruta_auth/priv/repo/boruta.seeds.exs
@@ -42,8 +42,7 @@ BorutaAuth.Repo.insert(
   on_conflict: :nothing
 )
 
-{:ok, client} = %Boruta.Ecto.Client{}
-|> Boruta.Ecto.Client.create_changeset(%{
+{:ok, client} = Boruta.Ecto.Admin.create_client(%{
   secret: System.get_env("BORUTA_ADMIN_OAUTH_CLIENT_SECRET", "777"),
   id: System.get_env("BORUTA_ADMIN_OAUTH_CLIENT_ID", "6a2f41a3-c54c-fce8-32d2-0324e1c32e20"),
   redirect_uris: [
@@ -54,17 +53,6 @@ BorutaAuth.Repo.insert(
   authorize_scope: true,
   authorized_scopes: BorutaAuth.Repo.all(Boruta.Ecto.Scope) |> Enum.map(&Map.from_struct/1)
 })
-|> BorutaAuth.Repo.insert(on_conflict: :nothing)
-
-BorutaGateway.Repo.insert(
-  %BorutaGateway.Upstreams.Upstream{
-    scheme: "http",
-    host: "localhost",
-    port: 4001,
-    uris: ["/"]
-  },
-  on_conflict: :nothing
-)
 
 {:ok, identity_provider} = BorutaIdentity.IdentityProviders.create_identity_provider(%{
   name: "Default",
@@ -75,21 +63,23 @@ BorutaIdentity.IdentityProviders.upsert_client_identity_provider(client.id, iden
 
 {:ok, user} = BorutaIdentity.Accounts.Internal.User.registration_changeset(%BorutaIdentity.Accounts.Internal.User{}, %{
   email: System.get_env("BORUTA_ADMIN_EMAIL", "test@test.test"),
-  password: System.get_env("BORUTA_ADMIN_PASSWORD", "passwordesat")
+  password: System.get_env("BORUTA_ADMIN_PASSWORD", "passwordesat"),
+  password_confirmation: System.get_env("BORUTA_ADMIN_PASSWORD", "passwordesat")
 }) |> BorutaIdentity.Repo.insert()
 
-scopes =
-  [
-    "users:manage:all",
-    "clients:manage:all",
-    "scopes:manage:all",
-    "upstreams:manage:all",
-    "identity-providers:manage:all",
-    "instances:manage:user"
-  ]
-  |> Enum.map(fn scope_name ->
-    BorutaIdentity.Repo.insert(%BorutaIdentity.Accounts.UserAuthorizedScope{
-      name: scope_name,
-      user_id: user.id
-    })
-  end)
+user = BorutaIdentity.Accounts.Internal.domain_user!(user)
+
+Boruta.Ecto.Admin.get_scopes_by_names([
+  "users:manage:all",
+  "clients:manage:all",
+  "scopes:manage:all",
+  "upstreams:manage:all",
+  "identity-providers:manage:all",
+  "instances:manage:user"
+])
+|> Enum.map(fn %{id: scope_id} ->
+  BorutaIdentity.Repo.insert(%BorutaIdentity.Accounts.UserAuthorizedScope{
+    scope_id: scope_id,
+    user_id: user.id
+  })
+end)
--- apps/boruta_gateway/priv/repo/seeds.exs
+++
@@ -1,8 +0,0 @@
-alias BorutaGateway.Upstreams.Upstream
-
-BorutaGateway.Repo.insert(%Upstream{
-  scheme: "http",
-  host: "localhost",
-  port: 4001,
-  uris: ["/"]
-})
