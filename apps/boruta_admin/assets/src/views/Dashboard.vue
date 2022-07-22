14ce10245c8511b49d8f1af9c410a384ed3cead5
Pascal Knoth <io.pascal.knoth@gmail.com>
2022-07-22 07:38:03 +0200

    [admin] dashboard's rpm graph

--- apps/boruta_admin/assets/src/views/Dashboard.vue
+++ apps/boruta_admin/assets/src/views/Dashboard.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="dashboard">
     <div class="container">
-      <GatewayRequests />
       <h2>Request logs</h2>
       <div class="ui logs segment">
-        <pre>{{ requestLogs }}</pre>
+        <pre>{{ requestLogs.join('\n') }}</pre>
+      </div>
+      <div class="ui one column stackable grid">
+        <div class="column request-time">
+          <LineChart :chartData="requestTimes" :options="requestTimesOptions" height="500" />
+        </div>
       </div>
       <h2>Business logs</h2>
       <div class="ui logs segment">
@@ -15,37 +19,94 @@
 </template>
 
 <script>
+import { LineChart } from "vue-chart-3";
 import Logs from '../services/logs.service'
 import GatewayRequests from '../components/GatewayRequests.vue'
 
-const REQUEST_REGEX = /([^\s]+) request_id=(\w+) \[info\] (\w+) (\w+) ([^\s]+) - (\w+) (\d{3}) in (\d+)(\w{2})/
-const BUSINESS_REGEX = /([^\s]+) request_id=(\w+) \[info\] (\w+) (\w+) - (\w+)( (\w+)=((\".+\")|([^\s]+)))+/
+const REQUEST_REGEX = /(\d{4}-\d{2}-\d{2}T[^\s]+Z) request_id=(\w+) \[info\] (\w+) (\w+) ([^\s]+) - (\w+) (\d{3}) in (\d+)(\w{2})/
+const BUSINESS_REGEX = /(\d{4}-\d{2}-\d{2}T[^\s]+Z) request_id=(\w+) \[info\] (\w+) (\w+) - (\w+)( (\w+)=((\".+\")|([^\s]+)))+/
 
 export default {
   name: 'home',
   components: {
-    GatewayRequests
+    LineChart
   },
   data() {
     return {
       // logs: '',
-      requestLogs: '',
-      businessLogs: ''
+      requestLogs: [],
+      businessLogs: '',
+      requestTimes: {
+        labels: [],
+        datasets: []
+      },
+      requestTimesOptions: {
+        plugins: {
+          title: {
+            display: true,
+            text: 'Requests per minute'
+          }
+        },
+        scales: {
+          xAxis: {
+            display: false
+          }
+        }
+      }
     }
   },
   async mounted() {
     const stream = await Logs.stream()
 
     const read = (stream) => {
-      console.log(stream)
-
       stream.read().then(({ done, value }) => {
         // decode Uint8Array to utf-8 string
         const data = new TextDecoder().decode(value)
 
         // this.logs += data
         data.split('\n').map(log => {
-          if (log.match(REQUEST_REGEX)) this.requestLogs += `${log}\n`
+          const requestMatches = log.match(REQUEST_REGEX)
+          if (requestMatches) {
+            this.requestLogs.push(`${log}`)
+            const time = new Date(requestMatches[1])
+            time.setMilliseconds(0)
+            time.setSeconds(0)
+            const application = requestMatches[3]
+            if (application == 'boruta_admin') return
+            const method = requestMatches[4]
+            const path = requestMatches[5]
+
+            const currentLabel = `${application} - ${method} ${path}`
+
+            const labels = this.requestTimes.labels
+            const lastLabel = labels.slice(-1)[0]
+            if (!lastLabel || !(lastLabel.getTime() == time.getTime())) {
+              labels.push(time)
+            }
+
+            let dataset = this.requestTimes.datasets.find(({ label }) => {
+              return label === currentLabel
+            })
+            if (!dataset) {
+              dataset = {
+                label: currentLabel,
+                borderColor: stringToColor(currentLabel),
+                fill: false,
+                lineTension: 0,
+                data: null
+              }
+              this.requestTimes.datasets.push(dataset)
+            }
+
+            const currentData = dataset.data || new Array()
+            const nextData = new Array(labels.length)
+            for (let i = 0; i < nextData.length; i++) {
+              nextData[i] = currentData[i] || 0
+            }
+            nextData.splice(-1, 1, nextData.slice(-1)[0] + 1)
+
+            dataset.data = nextData.map(value => value === 0 ? NaN : value)
+          }
           if (log.match(BUSINESS_REGEX)) this.businessLogs += `${log}\n`
         })
 
@@ -59,6 +120,19 @@ export default {
 
     read(stream)
   }
+}
+
+function stringToColor(str) {
+  let hash = 0
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash)
+  }
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xFF
+    colour += ('00' + value.toString(16)).substr(-2)
+  }
+  return colour
 }
 </script>
 
--- config/config.exs
+++ config/config.exs
@@ -5,15 +5,17 @@ for config <- "../apps/*/config/config.exs" |> Path.expand(__DIR__) |> Path.wild
 end
 
 config :logger,
+  utc_log: true,
   backends: [{LoggerFileBackend, :file_logger}, :console]
 
 config :logger, :file_logger,
+  format: "$dateT$timeZ $metadata[$level] $message\n",
   path: "./log/test",
   metadata: [:request_id],
   level: :info
 
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: "$dateT$timeZ $metadata[$level] $message\n",
   metadata: [:request_id]
 
 config :phoenix, :json_library, Jason
