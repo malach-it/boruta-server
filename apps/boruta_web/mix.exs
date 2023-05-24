43d86cd90f76d36dcc5e901b7cf5171430915256
Pascal Knoth <io.pascal.knoth@gmail.com>
2023-05-24 07:37:29 +0200

    bump gettext to 0.22.1

--- apps/boruta_web/mix.exs
+++ apps/boruta_web/mix.exs
@@ -10,7 +10,7 @@ defmodule BorutaWeb.MixProject do
       lockfile: "../../mix.lock",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
