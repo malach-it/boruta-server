38b1ed688748c35997895f9aa46ac9a673343936
Pascal Knoth <pascal@malach.it>
2024-04-27 19:39:04 +0200

    [infra] fix no user seeds

--- apps/boruta_auth/priv/repo/boruta.seeds.exs
+++ apps/boruta_auth/priv/repo/boruta.seeds.exs
@@ -90,76 +90,83 @@ BorutaAuth.Repo.insert(
 )
 
 client_id = System.get_env("BORUTA_ADMIN_OAUTH_CLIENT_ID", "6a2f41a3-c54c-fce8-32d2-0324e1c32e20")
-client = case Boruta.Ecto.Admin.create_client(%{
-    name: "Boruta administration panel",
-    secret: System.get_env("BORUTA_ADMIN_OAUTH_CLIENT_SECRET", "777"),
-    id: client_id,
-    redirect_uris: [
-      "#{System.get_env("BORUTA_ADMIN_BASE_URL", "http://localhost:4001")}/oauth-callback"
-    ],
-    access_token_ttl: 3600,
-    authorization_code_ttl: 60,
-    public_revoke: true
-}) do
-  {:ok, client} -> client
-  {:error, _error} -> Boruta.Ecto.Admin.get_client!(client_id)
-end
+
+client =
+  case Boruta.Ecto.Admin.create_client(%{
+         name: "Boruta administration panel",
+         secret: System.get_env("BORUTA_ADMIN_OAUTH_CLIENT_SECRET", "777"),
+         id: client_id,
+         redirect_uris: [
+           "#{System.get_env("BORUTA_ADMIN_BASE_URL", "http://localhost:4001")}/oauth-callback"
+         ],
+         access_token_ttl: 3600,
+         authorization_code_ttl: 60,
+         public_revoke: true
+       }) do
+    {:ok, client} -> client
+    {:error, _error} -> Boruta.Ecto.Admin.get_client!(client_id)
+  end
 
 backend = BorutaIdentity.IdentityProviders.Backend.default!()
 
 BorutaIdentity.IdentityProviders.create_identity_provider(%{
-    name: "Default",
-    registrable: true,
-    backend_id: backend.id
+  name: "Default",
+  registrable: true,
+  backend_id: backend.id
 })
 
-identity_provider = case BorutaIdentity.IdentityProviders.create_identity_provider(%{
-    name: "Boruta administration interface",
-    registrable: false,
-    backend_id: backend.id
-}) do
-  {:ok, identity_provider} -> identity_provider
-  {:error, _error} ->
-    BorutaIdentity.IdentityProviders.list_identity_providers()
-  |> Enum.find(fn (%{name: name}) -> name == "Boruta administration interface" end)
-end
+identity_provider =
+  case BorutaIdentity.IdentityProviders.create_identity_provider(%{
+         name: "Boruta administration interface",
+         registrable: false,
+         backend_id: backend.id
+       }) do
+    {:ok, identity_provider} ->
+      identity_provider
+
+    {:error, _error} ->
+      BorutaIdentity.IdentityProviders.list_identity_providers()
+      |> Enum.find(fn %{name: name} -> name == "Boruta administration interface" end)
+  end
 
 BorutaIdentity.IdentityProviders.upsert_client_identity_provider(client.id, identity_provider.id)
 
 email = System.get_env("BORUTA_ADMIN_EMAIL")
-user = case BorutaIdentity.Accounts.Internal.User.registration_changeset(
-    %BorutaIdentity.Accounts.Internal.User{},
-    %{
-      email: email,
-      password: System.get_env("BORUTA_ADMIN_PASSWORD"),
-      password_confirmation: System.get_env("BORUTA_ADMIN_PASSWORD"),
-      confirmed_at: DateTime.utc_now()
-    },
-    %{backend: backend}
-  )
-  |> BorutaIdentity.Repo.insert() do
-  {:ok, user} ->
-    user
-  {:error, _error} ->
-    BorutaIdentity.Repo.get_by(BorutaIdentity.Accounts.Internal.User, email: email)
-end
 
-user = BorutaIdentity.Accounts.Internal.domain_user!(user, backend)
+user =
+  case BorutaIdentity.Accounts.Internal.User.registration_changeset(
+         %BorutaIdentity.Accounts.Internal.User{},
+         %{
+           email: email,
+           password: System.get_env("BORUTA_ADMIN_PASSWORD"),
+           password_confirmation: System.get_env("BORUTA_ADMIN_PASSWORD"),
+           confirmed_at: DateTime.utc_now()
+         },
+         %{backend: backend}
+       )
+       |> BorutaIdentity.Repo.insert() do
+    {:ok, user} ->
+      user = BorutaIdentity.Accounts.Internal.domain_user!(user, backend)
 
-Boruta.Ecto.Admin.get_scopes_by_names([
-  "users:manage:all",
-  "clients:manage:all",
-  "scopes:manage:all",
-  "roles:manage:all",
-  "upstreams:manage:all",
-  "identity-providers:manage:all",
-  "configuration:manage:all",
-  "logs:read:all"
-])
-|> Enum.map(fn %{id: scope_id} ->
-  %BorutaIdentity.Accounts.UserAuthorizedScope{
-    scope_id: scope_id,
-    user_id: user.id
-  } |> Ecto.Changeset.change()
-  |> BorutaIdentity.Repo.insert(on_conflict: :nothing)
-end)
+      Boruta.Ecto.Admin.get_scopes_by_names([
+        "users:manage:all",
+        "clients:manage:all",
+        "scopes:manage:all",
+        "roles:manage:all",
+        "upstreams:manage:all",
+        "identity-providers:manage:all",
+        "configuration:manage:all",
+        "logs:read:all"
+      ])
+      |> Enum.map(fn %{id: scope_id} ->
+        %BorutaIdentity.Accounts.UserAuthorizedScope{
+          scope_id: scope_id,
+          user_id: user.id
+        }
+        |> Ecto.Changeset.change()
+        |> BorutaIdentity.Repo.insert(on_conflict: :nothing)
+      end)
+
+    {:error, _error} ->
+      nil
+  end
