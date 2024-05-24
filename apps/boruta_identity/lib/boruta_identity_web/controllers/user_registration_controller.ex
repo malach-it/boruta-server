0f4017e60f16d082d4901e48580a08b7992932c6
Pascal Knoth <pascal@malach.it>
2024-05-24 09:13:55 +0200

    [identity] rescue from bad confirmation email on registration

--- apps/boruta_identity/lib/boruta_identity_web/controllers/user_registration_controller.ex
+++ apps/boruta_identity/lib/boruta_identity_web/controllers/user_registration_controller.ex
@@ -70,6 +70,33 @@ defmodule BorutaIdentityWeb.UserRegistrationController do
     )
   end
 
+  @impl BorutaIdentity.Accounts.RegistrationApplication
+  def registration_failure(%Plug.Conn{} = conn, %RegistrationError{
+        message: message,
+        template: template
+      }) do
+    client_id = client_id_from_request(conn)
+
+    :telemetry.execute(
+      [:registration, :create, :failure],
+      %{},
+      %{
+        client_id: client_id,
+        error: message
+      }
+    )
+
+    conn
+    |> put_layout(false)
+    |> put_view(TemplateView)
+    |> render("template.html",
+      template: template,
+      assigns: %{
+        errors: [message]
+      }
+    )
+  end
+
   def registration_failure(%Plug.Conn{} = conn, %RegistrationError{
         user: user,
         message: message,
