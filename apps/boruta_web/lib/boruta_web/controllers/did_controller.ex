fb5497e31920769edb20c2fb9f1ab6ed2410e205
Pascal Knoth <pascal@malach.it>
2024-08-24 23:10:10 +0200

    [ssi] check status against all sever clients

--- apps/boruta_web/lib/boruta_web/controllers/did_controller.ex
+++ apps/boruta_web/lib/boruta_web/controllers/did_controller.ex
@@ -1,21 +1,18 @@
 defmodule BorutaWeb.DidController do
-  alias Boruta.BasicAuth
   use BorutaWeb, :controller
 
-  alias Boruta.ClientsAdapter
   alias Boruta.VerifiableCredentials
 
   def resolve_status(conn, %{"status" => salt}) do
-    case conn |> get_req_header("authorization") |> List.first() |> BasicAuth.decode() |> dbg do
-      {:ok, [did, _]} ->
-        client = ClientsAdapter.get_client_by_did(did)
+    clients = Boruta.Ecto.Admin.list_clients()
+    status = Enum.reduce_while(clients, :invaild, fn client, _acc ->
+      case VerifiableCredentials.verify_salt(client.private_key, salt) do
+        :expired -> {:cont, :expired}
+        :invalid -> {:cont, :invalid}
+        status -> {:halt, status}
+      end
+    end)
 
-        status = VerifiableCredentials.verify_salt(client.private_key, salt)
-
-        send_resp(conn, 200, Atom.to_string(status))
-
-      _ ->
-        send_resp(conn, 400, "Bad request")
-    end
+    send_resp(conn, 200, Atom.to_string(status))
   end
 end
