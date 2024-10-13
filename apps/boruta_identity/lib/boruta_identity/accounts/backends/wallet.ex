ac0dcf60b00cd4063f20ac1175d5c932cd5b3c50
Pascal Knoth <pascal@malach.it>
2024-10-13 11:36:38 +0200

    [ssi] fix direct post handler

---
+++ apps/boruta_identity/lib/boruta_identity/accounts/backends/wallet.ex
@@ -0,0 +1,30 @@
+defmodule BorutaIdentity.Accounts.Wallet do
+  alias BorutaIdentity.Accounts.User
+  alias BorutaIdentity.Repo
+
+  def domain_user!(resource_owner, backend) do
+    impl_user_params = %{
+      uid: resource_owner.sub,
+      username: resource_owner.sub,
+      backend_id: backend.id
+    }
+
+    metadata = resource_owner.extra_claims
+    {replace, impl_user_params} =
+      case metadata do
+        %{} = metadata ->
+          {[:username, :metadata, :group], Map.put(impl_user_params, :metadata, metadata)}
+
+        _ ->
+          {[:username, :group], impl_user_params}
+      end
+
+    User.implementation_changeset(impl_user_params, backend)
+    |> Repo.insert!(
+      on_conflict: {:replace, replace},
+      returning: true,
+      conflict_target: [:backend_id, :uid]
+    )
+    |> Repo.preload([:authorized_scopes, :consents, :backend, :organizations])
+  end
+end
