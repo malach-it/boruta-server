49c5c997a8e7b913ff5a1b31ec6d7b23a3889321
Pascal Knoth <pascal@malach.it>
2024-11-04 09:18:31 +0100

    [auth] add trust mark and required claims to trust chain statements

--- apps/boruta_federation/lib/boruta_federation/federation_entities/leaf_entity.ex
+++ apps/boruta_federation/lib/boruta_federation/federation_entities/leaf_entity.ex
@@ -1,6 +1,8 @@
 defmodule BorutaFederation.FederationEntities.LeafEntity do
   @moduledoc false
 
+  alias BorutaFederation.FederationEntities.FederationEntity
+
   import Boruta.Config, only: [issuer: 0]
 
   defmodule Token do
@@ -24,6 +26,26 @@ defmodule BorutaFederation.FederationEntities.LeafEntity do
   @spec jwks(entity :: FederationEntity.t()) :: {:ok, jwks :: map()}
   def jwks(entity) do
     {:ok, [JOSE.JWK.from_pem(entity.public_key) |> JOSE.JWK.to_map() |> elem(1)]}
+  end
+
+  @spec trust_marks(entity :: FederationEntity.t()) :: {:ok, trust_mark :: list(String.t())}
+  def trust_marks(entity) do
+    signer = Joken.Signer.create(entity.trust_chain_statement_alg, %{"pem" => entity.private_key})
+
+    payload = %{
+      "iss" => issuer(),
+      "sub" => issuer(),
+      "id" => entity.id,
+      "iat" => :os.system_time(:second),
+      "logo_uri" => entity.trust_mark_logo_uri
+    }
+
+    case Joken.encode_and_sign(payload, signer) do
+      {:ok, trust_mark, _payload} ->
+        {:ok, [trust_mark]}
+      {:error, error} ->
+        {:error, to_string(error)}
+    end
   end
 
   @spec resolve_parents_chain(entity :: FederationEntity.t()) :: {:ok, chain :: list(String.t())}
