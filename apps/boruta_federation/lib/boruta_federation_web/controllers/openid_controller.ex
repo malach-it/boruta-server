ccb812eafb5755b35b8800e4f4410b983044c4d1
Pascal Knoth <pascal@malach.it>
2025-02-05 17:03:15 +0100

    [federation] sign well known payload

--- apps/boruta_federation/lib/boruta_federation_web/controllers/openid_controller.ex
+++ apps/boruta_federation/lib/boruta_federation_web/controllers/openid_controller.ex
@@ -2,6 +2,6 @@ defmodule BorutaFederationWeb.OpenidController do
   use BorutaFederationWeb, :controller
 
   def well_known(conn, _params) do
-    render(conn, "well_known.json")
+    render(conn, "well_known.jwt")
   end
 end
--- apps/boruta_federation/lib/boruta_federation_web/views/openid_view.ex
+++ apps/boruta_federation/lib/boruta_federation_web/views/openid_view.ex
@@ -1,11 +1,18 @@
 defmodule BorutaFederationWeb.OpenidView do
   use BorutaFederationWeb, :view
 
-  def render("well_known.json", _params) do
-    # TODO change to the default entity statement
-    %{
-      federation_fetch_endpoint: Routes.fetch_url(BorutaFederationWeb.Endpoint, :fetch),
-      federation_resolve_endpoint: Routes.resolve_url(BorutaFederationWeb.Endpoint, :resolve)
-    }
+  alias BorutaFederation.TrustChains
+  alias BorutaFederation.FederationEntities
+
+  def render("well_known.jwt", _params) do
+    with {:ok, well_known, _claims} <- TrustChains.sign(
+      %{
+        federation_fetch_endpoint: Routes.fetch_url(BorutaFederationWeb.Endpoint, :fetch),
+        federation_resolve_endpoint: Routes.resolve_url(BorutaFederationWeb.Endpoint, :resolve)
+      },
+      FederationEntities.list_entities() |> List.first()
+    ) do
+      well_known
+    end
   end
 end
