8007a7a3fdf6add2ed7896936c661378308eaccc
Pascal Knoth <pascal@malach.it>
2025-03-20 11:41:28 +0100

    [admin] [wallet] fix npm vulnerabilities

--- apps/boruta_admin/assets/vite.config.js
+++ apps/boruta_admin/assets/vite.config.js
@@ -2,10 +2,31 @@ import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  define: {
+    'process.env.NODE_ENV': '"production"',
+  },
+  plugins: [
+    vue(),
+    viteSingleFile(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
+  ],
   build: {
     outDir: path.resolve(__dirname, '../priv/static/assets'),
     lib: {
@@ -17,12 +38,6 @@ export default defineConfig({
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js'
-      }
-    }
+    brotliSize: false
   }
 })
--- apps/boruta_identity/assets/wallet/vite.config.js
+++ apps/boruta_identity/assets/wallet/vite.config.js
@@ -69,12 +69,6 @@ export default defineConfig({
     assetsInlineLimit: 100000000,
     chunkSizeWarningLimit: 100000000,
     cssCodeSplit: false,
-    brotliSize: false,
-    rollupOptions: {
-      inlineDynamicImports: true,
-      output: {
-        manualChunks: () => 'everything.js'
-      }
-    }
+    brotliSize: false
   }
 })
