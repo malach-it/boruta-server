e2d36d5e7cb3e57554ac7e693c10d80f3219ea8b
Pascal Knoth <pascal@malach.it>
2025-05-24 15:07:07 +0200

    [wallet] fix QR code url navigation

--- apps/boruta_identity/assets/wallet/src/views/HomeView.vue
+++ apps/boruta_identity/assets/wallet/src/views/HomeView.vue
@@ -39,7 +39,7 @@ export default defineComponent({
       const url = new URL(result)
       this.qrScanner?.stop()
       this.scanning = false
-      this.$router.push(url.pathname + url.search)
+      this.$router.push(url.pathname.replace('/accounts/wallet', '') + url.search)
     })
   },
   computed: {
