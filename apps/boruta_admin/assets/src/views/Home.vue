87c4aa72ddc9642b68a1e5a8407ea60d3fa630be
Pascal Knoth <pascal@malach.it>
2025-05-25 20:50:07 +0200

    [admin] wallet demonstration link on home page

--- apps/boruta_admin/assets/src/views/Home.vue
+++ apps/boruta_admin/assets/src/views/Home.vue
@@ -63,12 +63,26 @@
           <p>Use integrated <a target="_blank" :href="walletUrl">web wallet</a> to request, store, and present verifiable credentials</p>
           <p>In order to execute example decentralized identity flows, you must generate client did first - <router-link :to="{ name: 'edit-client', params: { clientId: '00000000-0000-0000-0000-000000000001' } }">client configuration</router-link></p>
           <hr />
-          <div class="ui segment">
+          <div class="ui form segment">
             <h3>Verifiable credential issuance</h3>
+            <div class="field">
+              <label>Select a wallet</label>
+              <select v-model="issuanceRedirectUri">
+                <option :value="walletRedirectUri + '/preauthorized-code'">Internal wallet</option>
+                <option value="openid-credential-offer://">Mobile wallet</option>
+              </select>
+            </div>
             <a class="ui fluid blue button" target="_blank" :href="preauthorizeUrl">Trigger example pre-authorized code flow with associated boruta wallet (load example data first)</a>
           </div>
-          <div class="ui segment">
+          <div class="ui form segment">
             <h3>Verifiable credential presentation</h3>
+            <div class="field">
+              <label>Select a wallet</label>
+              <select v-model="presentationRedirectUri">
+                <option :value="walletRedirectUri + '/verifiable-presentation'">Internal wallet</option>
+                <option value="openid4vp://">Mobile wallet</option>
+              </select>
+            </div>
             <a class="ui fluid blue button" target="_blank" :href="presentationUrl">Trigger example presentation with associated boruta wallet (issue example credential first)</a>
           </div>
         </div>
@@ -80,18 +94,30 @@
 <script>
 export default {
   name: 'home',
+  data () {
+    return {
+      walletRedirectUri: `${window.env.BORUTA_OAUTH_BASE_URL}/accounts/wallet`,
+      issuanceRedirectUri: `${window.env.BORUTA_OAUTH_BASE_URL}/accounts/wallet/preauthorized-code`,
+      presentationRedirectUri: `${window.env.BORUTA_OAUTH_BASE_URL}/accounts/wallet/verifiable-presentation`
+    }
+  },
   computed: {
     walletUrl () {
      return window.env.BORUTA_OAUTH_BASE_URL +
       '/accounts/wallet'
     },
     presentationUrl () {
-     return window.env.BORUTA_OAUTH_BASE_URL +
-      `/oauth/authorize?client_id=00000000-0000-0000-0000-000000000001&redirect_uri=${window.env.BORUTA_OAUTH_BASE_URL}/accounts/wallet/verifiable-presentation&scope=BorutaCredentialJwtVc&response_type=vp_token&client_metadata={}&state=qrm0c4xm&prompt=login`
+      if (this.presentationRedirectUri === this.walletRedirectUri) {
+        return window.env.BORUTA_OAUTH_BASE_URL +
+          `/oauth/authorize?client_id=00000000-0000-0000-0000-000000000001&redirect_uri=${this.presentationRedirectUri}&scope=BorutaCredentialJwtVc&response_type=code&client_metadata={}&prompt=login`
+      } else {
+        return window.env.BORUTA_OAUTH_BASE_URL +
+          `/oauth/authorize?client_id=00000000-0000-0000-0000-000000000001&redirect_uri=${this.presentationRedirectUri}&scope=BorutaCredentialJwtVc&response_type=vp_token&client_metadata={}&prompt=login`
+      }
     },
     preauthorizeUrl () {
      return window.env.BORUTA_OAUTH_BASE_URL +
-      `/oauth/authorize?client_id=00000000-0000-0000-0000-000000000001&redirect_uri=${window.env.BORUTA_OAUTH_BASE_URL}/accounts/wallet/preauthorized-code&scope=BorutaCredentialJwtVc&response_type=urn%3Aietf%3Aparams%3Aoauth%3Aresponse-type%3Apre-authorized_code&state=qrm0c4xm&prompt=login`
+      `/oauth/authorize?client_id=00000000-0000-0000-0000-000000000001&redirect_uri=${this.issuanceRedirectUri}&response_type=urn%3Aietf%3Aparams%3Aoauth%3Aresponse-type%3Apre-authorized_code&state=qrm0c4xm&prompt=login`
     }
   }
 }
