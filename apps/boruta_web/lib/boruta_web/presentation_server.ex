5ff28ddd956cc2a50fc2ea85faa63a2ae5aac18b
Pascal Knoth <pascal@malach.it>
2025-06-24 03:49:17 +0200

    [ssi] same device qr codes display

---
+++ apps/boruta_web/lib/boruta_web/presentation_server.ex
@@ -0,0 +1,40 @@
+defmodule BorutaWeb.PresentationServer do
+  @moduledoc false
+
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init(_args) do
+    {:ok, %{presentations: %{}}}
+  end
+
+  def start_presentation(code) do
+    GenServer.call(__MODULE__, {:start_presentation, code})
+  end
+
+  def authenticated(code, redirect_uri) do
+    GenServer.cast(__MODULE__, {:authenticated, code, redirect_uri})
+  end
+
+  def handle_call({:start_presentation, code}, {pid, _}, state) do
+    presentations = Map.put(
+      state.presentations,
+      code,
+      %{
+        start: :os.system_time(:microsecond),
+        pid: pid
+      }
+    )
+    {:reply, :ok, %{state| presentations: presentations}}
+  end
+
+  def handle_cast({:authenticated, code, redirect_uri}, state) do
+    presentation = state.presentations[code]
+    send(presentation[:pid], {:authenticated, redirect_uri})
+
+    {:noreply, Map.delete(state, code)}
+  end
+end
