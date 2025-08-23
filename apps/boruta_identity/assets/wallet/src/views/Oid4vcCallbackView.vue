e5945d8c6417c4f07e25b8bf6068adc8cb2700a8
Pascal Knoth <pascal@malach.it>
2025-08-23 18:49:23 +0200

    [wallet] remove key selection from session at issuance

--- apps/boruta_identity/assets/wallet/src/views/Oid4vcCallbackView.vue
+++ apps/boruta_identity/assets/wallet/src/views/Oid4vcCallbackView.vue
@@ -36,7 +36,7 @@
     <div v-if="credentials.length">
       <Credentials :credentials="credentials" delete-label="Unselect" @deleteCredential="deleteCredential" />
       <div class="ui segment">
-        <form :action="redirect_uri" method="POST" @submit="localStorage.removeItem('keySelection')">
+        <form :action="redirect_uri" method="POST"  @submit="resetKeySelect">
           <input type="hidden" name="vp_token" :value="vp_token" />
           <input type="hidden" name="presentation_submission" :value="presentation_submission" />
           <button class="ui violet large fluid button" type="submit">Present your credential to {{ host }}</button>
@@ -210,6 +210,9 @@ export default defineComponent({
     },
     abortKeyConsent () {
       this.generateKeyConsentEventKey = null
+    },
+    resetKeySelect () {
+      localStorage.removeItem('keySelection')
     }
   },
   watch: {
