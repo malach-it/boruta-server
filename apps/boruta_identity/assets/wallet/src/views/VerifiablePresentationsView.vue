84e3726731ba464e6508f959a8cc16d28ef2b337
Pascal Knoth <pascal@malach.it>
2025-08-23 13:22:37 +0200

    [wallet] restrict siopv2 id token usage to selected key

--- apps/boruta_identity/assets/wallet/src/views/VerifiablePresentationsView.vue
+++ apps/boruta_identity/assets/wallet/src/views/VerifiablePresentationsView.vue
@@ -45,15 +45,7 @@
     <div class="ui segment" v-if="id_token">
       <form method="POST" :action="redirect_uri" class="ui large segment">
         <input type="hidden" name="response" :value="response" />
-        <textarea name="metadata_policy">
-{
-  "client_id": {
-    "one_of": [
-      "did:key:z2dmzD81cgPx8Vki7JbuuMmFYrWPgYoytykUZ3eyqht1j9KbpVe9GCARGebSWmYgPcKDDqKUqCCB5q5PLeQtp5yonRSDw7mgFESbYmELAa9kS4pfUAfqMAg4ZhnQXnwcHRrgCS9iY6ddeQu9jxGsqyNdNHiRouPKUiyFp2ZkJhQk2itdmE"
-    ]
-  }
-}
-        </textarea>
+        <input type="hidden" name="metadata_policy" :value="metadata_policy">
         <button class="ui fluid blue button" type="submit">Present your cryptographic key</button>
       </form>
     </div>
@@ -95,6 +87,7 @@ export default defineComponent({
       presentation: null,
       credentials: [],
       id_token: null,
+      metadata_policy: "{}",
       redirect_uri: null,
       vp_token: null,
       response: null,
@@ -179,8 +172,9 @@ export default defineComponent({
     console.log(this.mode)
   },
   methods: {
-    async selectKey (identifier) {
+    async selectKey (identifier, did) {
       this.selectedKey = identifier
+      this.metadata_policy = JSON.stringify({ client_id: { one_of: [ did ] } })
       eventHandler.dispatch('extract_key-approval', this.keyConsentEventKey, identifier)
       this.keyConsentEventKey = null
     },
