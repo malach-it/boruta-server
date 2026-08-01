0010b67a833afaf309e1b6ea008a698def9e57d2
Pascal Knoth <pascal@malach.it>
2026-08-01 19:18:03 +0200

    [identity] fix rebase conflict

--- apps/boruta_admin/lib/boruta_admin_web/controllers/identity_provider_controller.ex
+++ apps/boruta_admin/lib/boruta_admin_web/controllers/identity_provider_controller.ex
@@ -82,21 +82,11 @@ defmodule BorutaAdminWeb.IdentityProviderController do
   end
 
   def delete_template(conn, %{"identity_provider_id" => id, "template_type" => template_type}) do
-<<<<<<< HEAD
     case Template.fetch_template_type(template_type) do
       {:ok, template_type} ->
         template = IdentityProviders.delete_identity_provider_template!(id, template_type)
         render(conn, "show_template.json", template: template)
-
-      :error ->
-        raise Ecto.NoResultsError, queryable: Template
-=======
-    with {:ok, template_type} <- Template.fetch_template_type(template_type) do
-      template = IdentityProviders.delete_identity_provider_template!(id, template_type)
-      render(conn, "show_template.json", template: template)
-    else
       :error -> raise Ecto.NoResultsError, queryable: Template
->>>>>>> b38a7660 ([security] avoid atom creation from request values)
     end
   end
 
