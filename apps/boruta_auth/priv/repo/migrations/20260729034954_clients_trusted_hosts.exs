273dd503f9138498b2a9715ca39d26ed67121a41
Pascal Knoth <pascal@malach.it>
2026-08-01 19:14:37 +0200

    [auth] add trusted hosts / authorities database migrations

---
+++ apps/boruta_auth/priv/repo/migrations/20260729034954_clients_trusted_hosts.exs
@@ -0,0 +1,5 @@
+defmodule BorutaAuth.Repo.Migrations.ClientsTrustedHosts do
+  use Ecto.Migration
+
+  use Boruta.Migrations.ClientsTrustedHosts
+end
---
+++ apps/boruta_auth/priv/repo/migrations/20260801171454_clients_trusted_authorities.exs
@@ -0,0 +1,5 @@
+defmodule BorutaAuth.Repo.Migrations.ClientsTrustedAuthorities do
+  use Ecto.Migration
+
+  use Boruta.Migrations.ClientsTrustedAuthorities
+end
